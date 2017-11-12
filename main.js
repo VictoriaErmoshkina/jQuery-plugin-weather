e4f81fc12c6fe1b29b007f85578edb1b3cbdf393
Ermoshkina Victoria <ezhevika.96@gmail.com>
2017-11-12 12:45:58 +0300

    main.js was refactored.

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
  */
 var cities = ["Paris", "Oslo", "Zurich", "London", "Bangkok", "New York", "Honk Kong", "Los Angeles", "Tokyo", "San Francisco", "Vatican City"];
 var documentLists = $("ul");
-var returnedList = [];
-if (documentLists.eq(0)){
-    returnedList = documentLists.eq(0).weatherPlugin({
+if (documentLists.eq(0)) {
+    documentLists.eq(0).weatherPlugin({
         cities: cities,
-        direction: "down"});
+        direction: "down"
+    });
 } else
     console.log("Warning: Lists were not found on the page.");
